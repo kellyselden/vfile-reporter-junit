c399787339f8377685e712aecd35faeb06f17bb1
Kelly Selden <kellyselden@gmail.com>
2022-03-02 19:14:05 -0800

    support eslint 8
    This is a breaking change, and also requires eslint 8. vfileReporterJunit is now async.

--- test/index-test.js
+++ test/index-test.js
@@ -7,14 +7,14 @@ const remark = require('remark');
 const recommended = require('remark-preset-lint-recommended');
 
 describe(vfileReporterJunit, function() {
-  it('works', function() {
+  it('works', async function() {
     let file = remark()
       .use(recommended)
       .processSync('## Hello world!');
 
     file.path = '/foo/bar.baz';
 
-    expect(vfileReporterJunit([file])).to.equal(`<?xml version="1.0" encoding="utf-8"?>
+    expect(await vfileReporterJunit([file])).to.equal(`<?xml version="1.0" encoding="utf-8"?>
 <testsuites>
 <testsuite package="org.eslint" time="0" tests="1" errors="1" name="/foo/bar.baz">
 <testcase time="0" name="org.eslint.remark-lint:final-newline" classname="/foo/bar"><failure message="Missing newline character at end of file"><![CDATA[line 1, col 1, Warning - Missing newline character at end of file (remark-lint:final-newline)]]></failure></testcase>
